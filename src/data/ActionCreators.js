ab7d988a8628d1cc3495bb1732036f688c6fba8f
Sangeetha Kanna <sangeetha.kanna@hcl.com>
2021-03-09 16:37:14 +0530

    Pagination | Checkout | Validation | Code simplification | Others

--- src/data/ActionCreators.js
+++ src/data/ActionCreators.js
@@ -1,4 +1,4 @@
-import { ActionTypes } from "./Types";
+import { ActionTypes, DataTypes } from "./Types";
 import { RestDataSource } from "./RestDataSource";
 
 const dataSource = new RestDataSource();
@@ -14,3 +14,17 @@ export const loadData = (dataType, params) => ({
       params,
     })),
 });
+
+export const setPageSize = (newSize) =>
+    ({ type: ActionTypes.DATA_SET_PAGESIZE, payload: newSize});
+
+export const setSortProperty = (newProp) =>
+    ({ type: ActionTypes.DATA_SET_SORT_PROPERTY, payload: newProp});
+
+export const placeOrder = (order) => ({
+    type: ActionTypes.DATA_STORE,
+    payload: dataSource.StoreData(DataTypes.ORDERS, order).then(response => ({
+        dataType: DataTypes.ORDERS, data: response.data
+    }))
+});
+
