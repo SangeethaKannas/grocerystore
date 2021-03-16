ad470d05a2d0c13a775fbd3ccb379e3924bc42ce
Sangeetha Kanna <sangeetha.kanna@hcl.com>
2021-03-16 13:51:12 +0530

    Products and Orders

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Switch, Redirect }
     from "react-router-dom";
 import { ShopConnector } from "./components/shop/ShopConnector";
+import { Admin } from "./admin/Admin";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Router>
         <Switch>
             <Route path="/shop" component={ ShopConnector } />
+            <Route path="/admin" component={ Admin } />
             <Redirect to="/shop" />
         </Switch>
     </Router>
